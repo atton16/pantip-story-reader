e100236866d0ad0ca7abd7f9a284f2e4b2fd93de
Attawit Kittikrairit <atton16@gmail.com>
2017-07-19 12:46:26 +0700

    cleanup

--- frontend/src/pantip.js
+++ frontend/src/pantip.js
@@ -1,3 +1,5 @@
+var __DATAENV = 'prod'; // prod, dev, mock
+
 function extractPost(topicId, $data) {
   var post = {
     title: $data.find('#topic-'+topicId+' .display-post-title').html(),
@@ -24,43 +26,57 @@ function extractComments(topicId, data) {
 
 var $pantip = {
   retrievePost: function(topicId, cb){
-    // Actual retrieve
-    // var url = 'https://pantip.com/topic/' + topicId + '/story';
+    var url = '';
+    if (__DATAENV === 'prod') {
+      url = location.protocol + '//' + location.hostname + '/pantip-story-reader/story/' + topicId;
+    } else if (__DATAENV === 'dev') {
+      url = 'https://pantip.com/topic/' + topicId + '/story';
+    }
     // var url = 'http://13.229.61.182/story/' + topicId;
     // var url = 'https://us-central1-pantip-story-reader-76bfe.cloudfunctions.net/story?id=' + topicId;
-    var url = location.protocol + '//' + location.hostname + '/pantip-story-reader/story/' + topicId;
-    $.ajax({
-      url: url,
-      type: 'get',
-      success: cb
-    });
-    
-    // Mock response
-    // $.ajax({
-    //   url: './mock/post.html',
-    //   type: 'get',
-    //   success: cb
-    // });
+
+    // Actual retrieve
+    if (__DATAENV === 'prod' || __DATAENV === 'dev') {
+      $.ajax({
+        url: url,
+        type: 'get',
+        success: cb
+      });
+    } else {
+      // Mock response
+      $.ajax({
+        url: './mock/comments.json',
+        type: 'get',
+        success: cb
+      });
     
   },
   retrieveComments: function(topicId, cb){
-    // Actual retrieve
-    // var url = 'https://pantip.com/forum/topic_mode/render_comments?tid=' + topicId;
+    var url = '';
+    if (__DATAENV === 'prod') {
+      url = location.protocol + '//' + location.hostname + '/pantip-story-reader/comments/' + topicId;
+    } else if (__DATAENV === 'dev') {
+      url = 'https://pantip.com/forum/topic_mode/render_comments?tid=' + topicId;
+    }
     // var url = 'http://13.229.61.182/comments/' + topicId;
     // var url = 'https://us-central1-pantip-story-reader-76bfe.cloudfunctions.net/comments?id=' + topicId;
-    var url = location.protocol + '//' + location.hostname + '/pantip-story-reader/comments/' + topicId;
-    $.ajax({
-      url: url,
-      type: 'get',
-      success: cb
-    });
+
+    // Actual retrieve
+    if (__DATAENV === 'prod' || __DATAENV === 'dev') {
+      $.ajax({
+        url: url,
+        type: 'get',
+        success: cb
+      });
+    } else {
+      // Mock response
+      $.ajax({
+        url: './mock/comments.json',
+        type: 'get',
+        success: cb
+      });
+    }
     
-    // Mock response
-    // $.ajax({
-    //   url: './mock/comments.json',
-    //   type: 'get',
-    //   success: cb
-    // });
 
   },
   extractPost: function(topicId, cb){
